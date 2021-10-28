6dc32852b4b4ca9e7657d40d2cacc336338227d2
Boris Okunskiy <boris@okunskiy.name>
2021-10-28 17:47:11 +0300

    feat: allow defining string metadata

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,13 +1,13 @@
 import 'reflect-metadata';
 
-export function addClassMetadata<T>(key: Symbol, target: any, datum: T) {
+export function addClassMetadata<T>(key: Symbol | string, target: any, datum: T) {
     const _target = target instanceof Function ? target.prototype : target;
     const metadata = Reflect.getOwnMetadata(key, _target) || [];
     metadata.push(datum);
     Reflect.defineMetadata(key, metadata, _target);
 }
 
-export function getClassMetadata<T>(key: Symbol, target: any): T[] {
+export function getClassMetadata<T>(key: Symbol | string, target: any): T[] {
     const _target = target instanceof Function ? target.prototype : target;
     let result: T[] = [];
     let proto = _target;
